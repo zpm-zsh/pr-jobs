e6bb2a0f37a259753022acb28b7dfad2bb36fba8
Grigorii Horos <horosgrisa@gmail.com>
2019-08-09 00:47:43 +0300

    Fix colors

--- pr-jobs.plugin.zsh
+++ pr-jobs.plugin.zsh
@@ -13,12 +13,8 @@ _pr_jobs() {
   local jobs=$( jobs | wc -l )
   
   if [[ $jobs -gt 0 ]] ; then
-
-    if [[ $CLICOLOR = 1 ]]; then
-      pr_jobs="$PR_JOBS_PREFIX%{$c[blue]$c_bold$c_dim%}$PR_JOBS_SYMBOL%{$c_reset%}$PR_JOBS_SUFFIX"
-    else
-      pr_jobs="$PR_JOBS_PREFIX$PR_JOBS_SYMBOL$PR_JOBS_SUFFIX"
-    fi
+    
+    pr_jobs="$PR_JOBS_PREFIX%{$c[blue]$c_bold$c_dim%}$PR_JOBS_SYMBOL%{$c_reset%}$PR_JOBS_SUFFIX"
     
   else
     pr_jobs=''
