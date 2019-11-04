ffaf9bebae70d917e3c5546d8c9bc21df44d2158
Grigorii Horos <horosgrisa@gmail.com>
2019-11-04 12:16:26 +0200

    Fix

--- pr-jobs.plugin.zsh
+++ pr-jobs.plugin.zsh
@@ -4,23 +4,18 @@
 
 DEPENDENCES_ZSH+=( zpm-zsh/colors )
 
-if command -v zpm >/dev/null; then
+if (( $+functions[zpm] )); then
   zpm zpm-zsh/colors
 fi
 
 _pr_jobs() {
-  
   local jobs=$( jobs | wc -l )
   
   if [[ $jobs -gt 0 ]] ; then
-    
     pr_jobs="$PR_JOBS_PREFIX%{$c[blue]$c_bold$c_dim%}$PR_JOBS_SYMBOL%{$c_reset%}$PR_JOBS_SUFFIX"
-    
   else
     pr_jobs=''
   fi
-  
 }
-
 
 precmd_functions+=(_pr_jobs)
